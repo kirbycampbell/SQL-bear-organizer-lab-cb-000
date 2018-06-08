0dc10f15534883c1425e9184ec5b9d5388e35e68
Kirby Campbell <jkirbycampbell@gmail.com>
2018-06-08 17:08:24 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tim", 13,"M","Brown","Calm",true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tim", 13,"M","Brown","Calm",true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tim", 13,"M","Brown","Calm",true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tim", 13,"M","Brown","Calm",true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tim", 13,"M","Brown","Calm",true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tim", 13,"M","Brown","Calm",true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tim", 13,"M","Brown","Calm",true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tim", 13,"M","Brown","Calm",true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tim", 13,"M","Brown","Calm",1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tim", 13,"M","Brown","Calm",1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tim", 13,"M","Brown","Calm",1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tim", 13,"M","Brown","Calm",1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tim", 13,"M","Brown","Calm",0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tim", 13,"M","Brown","Calm",1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tim", 13,"M","Brown","Calm",1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tim", 13,"M","Brown","Calm",1);
