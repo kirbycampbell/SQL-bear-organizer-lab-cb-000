f46dceb286a2e25b08871dc2e79c2d13c53a2485
Kirby Campbell <jkirbycampbell@gmail.com>
2018-06-08 17:10:20 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tim", 13,"M","Brown","Calm",1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Dan", 16,"M","Blue","Wild",1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 16,"M","Blue","Wild",1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sam", 54,"M","Brown","Cray",1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tif", 13,"F","Yellow","Lit",1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Chad", 1,"M","Black","Calm",0);
